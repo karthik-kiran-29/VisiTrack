498a749db5246d99530f62213d75a487236b2ef5
karthik-kiran-29 <karthikkiran029@gmail.com>
2025-03-20 19:55:05 +0530

    added UserListsLayout

--- frontend/src/layouts/UserLinksLayout.jsx
+++ frontend/src/layouts/UserLinksLayout.jsx
@@ -6,13 +6,16 @@ const UserLinksLayout = ({UserName}) =>{
     const test = {LabelName:"sample",LabelLink:"google"}
     return(
         <div>
-            <div><div>{UserName}</div> <IconComponent {...{ActionName:"Add New Link",ActionColor:"border-black border-2 bg-blue-400"}}/></div>
+            <div className="flex md:p-10 p-5 flex-row justify-between ">
+                <div>{"Hello, " + UserName}</div> 
+                <IconComponent {...{ActionName:"Add New Link",ActionColor:"border-black border-2 bg-blue-400"}}/>
+            </div>
             <div className='p-4'>
-            <LabelComponent  {...test} key={1}/>
-            <LabelComponent  {...test}  key={2}/>
-            <LabelComponent  {...test} key={3}/>
-            <LabelComponent  {...test} key={4}/>
-            <LabelComponent  {...test} key={5}/>
+            <LabelComponent  {...test} />
+            <LabelComponent  {...test} />
+            <LabelComponent  {...test} />
+            <LabelComponent  {...test} />
+            <LabelComponent  {...test} />
             </div>
         </div>
     )
